e8a38d3bd5dee29b25b3f68b01fbe8f3ca971aaf
JmRivGonza <Jonaymartinrivero@gmail.com>
2026-03-20 15:08:41 +0000

    Creacion de la estructura base a partir de start.spring.io

---
+++ librosapp/src/main/java/com/damw/librosapp/LibrosappApplication.java
@@ -0,0 +1,13 @@
+package com.damw.librosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosappApplication.class, args);
+	}
+
+}
---
+++ librosapp/src/test/java/com/damw/librosapp/LibrosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damw.librosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
